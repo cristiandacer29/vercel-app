afbcf69073b2e6c796fc0944c4d8fabe556d56c7
cristiandacer29 <cristiandacer29@gmail.com>
2026-08-05 20:26:54 +0800

    Update visitor-counter.php

--- api/visitor-counter.php
+++ api/visitor-counter.php
@@ -54,7 +54,7 @@ setcookie('visitor_name987654321', $cleanName, [
 //----------------------------------------------------
 
 //save name in json file
-$jsonFile = 'counter-record.json';
+$jsonFile = '/tmp/counter-record.json';
 
 // Read existing data
 if (file_exists($jsonFile)) {
